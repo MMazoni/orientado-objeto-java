12d771c002fd3f17976a3e4b674a31463cc6f149
MMazoni <mmazoni.andrade@gmail.com>
2019-10-06 17:38:36 -0300

    fixed cachorro class

--- src/aula07/Cachorro.java
+++ src/aula07/Cachorro.java
@@ -3,6 +3,10 @@ package aula07;
 public class Cachorro extends Animal{
 
 	
+	public Cachorro(String nome, int idade) {
+		super(nome, idade);
+	}
+
 	public void latir() {
 		System.out.println("auau");
 	}
