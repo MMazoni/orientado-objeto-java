5b18849fb9740dc37964f1a90ab0b4fad5b9b3c8
MMazoni <mmazoni.andrade@gmail.com>
2019-10-06 17:38:08 -0300

    fixed gato class

--- src/aula07/Gato.java
+++ src/aula07/Gato.java
@@ -2,6 +2,10 @@ package aula07;
 
 public class Gato extends Animal{
 	
+	public Gato(String nome, int idade) {
+		super(nome, idade);
+	}
+
 	public void arranhar() {
 		System.out.println("zararapo");
 	}
