38124743290f90abf3066e40311b4ebffc4cfdb2
matheus.andrade <estagiario.sistemas@portodesantos.com.br>
2019-10-09 14:40:33 -0300

    add simulado ex1

---
+++ src/simulado/ex1/Produto.java
@@ -0,0 +1,15 @@
+package simulado.ex1;
+
+public class Produto {
+    private String nome;
+    private double preco;
+
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public void mostrarProduto(){
+        System.out.println("Produto  |" + nome + "|" + preco + "|");
+    }
+}
---
+++ src/simulado/ex1/Teste.java
@@ -0,0 +1,9 @@
+package simulado.ex1;
+
+public class Teste {
+    public static void main(String[] args) {
+        Produto p = new Produto("banana", 5.3);
+        p.mostrarProduto();
+
+    }
+}
